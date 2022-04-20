a87022ea989ad5d15ce12bde833fccb6e559b765
adiska21 <adislan2017@gmail.com>
2022-04-20 17:33:43 +0300

    БОТ ТГ ОКОНЧЕН ДЛЯ ЮТУБА!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ TG Bot by Adislan/download.py
@@ -0,0 +1,14 @@
+import youtube_dl
+import re
+
+def worker(VID_ID):
+
+	ydl_opts = {
+		'max_filesize': 20000000000,
+		'format': 'best',
+		'outtmpl': VID_ID + '.mp4',
+		'output': VID_ID + '.mp4',
+		'quiet': True
+	}
+	with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+		ydl.download([VID_ID])
